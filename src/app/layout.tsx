51e1ecaddfeff2c3bf53cec8988fafee867099bd
Mete <mete.huseynli2000@gmail.com>
2025-07-20 23:41:36 -0400

    design added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import ".src/app/globals.css"; // Path might vary, but MUST be imported in layout.tsx
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
